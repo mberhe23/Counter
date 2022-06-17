7cb5f1613f0b6495e87f6a07f9abfb16014ac62d
Maryan Berhe <maryanberhe@gmail.com>
2022-06-17 17:49:17 -0500

    Added to HTML, CSS, and Javascript files

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let count = 0
+count = count + 1
+console.log(count)
+
+// intitalize count as 0
+// listent for clicks on button
+// increment count when button is clicked
+// change count-element in html
+
+function increment() {
+    console.log("The button was clicked")
+}
